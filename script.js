2b9cca4924051447ad803a68bc3e47df8060aad7
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2025-11-16 10:37:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const appl=document.querySelector(".app");
+const boo=document.querySelector(".boo");
+const gasp=document.querySelector(".gas");
+const tada=document.querySelector(".tad");
+const victory=document.querySelector(".vic");
+const wrong=document.querySelector(".wro");
+const stop=document.querySelector(".sto");
+
+
+appl.addEventListener("click", ()=>{
+	const apSound=new Audio("sounds/applause.mp4");
+	apSound.play();
+});
+
+boo.addEventListener("click", ()=>{
+	
+});
+
+gasp.addEventListener("click", ()=>{
+	
+});
+
+tada.addEventListener("click", ()=>{
+	
+});
+
+victory.addEventListener("click", ()=>{
+	
+});
+
+wrong.addEventListener("click", ()=>{
+	
+});
+
+stop.addEventListener("click", ()=>{
+	
+});
