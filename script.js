b28742fa460e6cfdfb2535ace0672338fb10c1bd
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2025-11-16 12:22:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 //your JS code here. If required.
-const appl=document.querySelector(".app");
-const boo=document.querySelector(".boo");
-const gasp=document.querySelector(".gas");
-const tada=document.querySelector(".tad");
-const victory=document.querySelector(".vic");
-const wrong=document.querySelector(".wro");
-const stop=document.querySelector(".sto");
-
-
-appl.addEventListener("click", ()=>{
-	const apSound=new Audio("sounds/applause.mp4");
-	apSound.play();
-});
-
-boo.addEventListener("click", ()=>{
+document.addEventListener("DOMContentLoaded", ()=>{
+	const appl=document.querySelector(".app");
+	const boo=document.querySelector(".boo");
+	const gasp=document.querySelector(".gas");
+	const tada=document.querySelector(".tad");
+	const victory=document.querySelector(".vic");
+	const wrong=document.querySelector(".wro");
+	const stop=document.querySelector(".sto");
 	
-});
-
-gasp.addEventListener("click", ()=>{
 	
-});
-
-tada.addEventListener("click", ()=>{
+	appl.addEventListener("click", ()=>{
+		const apSound=new Audio("sounds/applause.mp4");
+		apSound.play();
+	});
 	
-});
-
-victory.addEventListener("click", ()=>{
+	boo.addEventListener("click", ()=>{
+		
+	});
 	
-});
-
-wrong.addEventListener("click", ()=>{
+	gasp.addEventListener("click", ()=>{
+		
+	});
 	
-});
-
-stop.addEventListener("click", ()=>{
+	tada.addEventListener("click", ()=>{
+		
+	});
 	
+	victory.addEventListener("click", ()=>{
+		
+	});
+	
+	wrong.addEventListener("click", ()=>{
+		
+	});
+	
+	stop.addEventListener("click", ()=>{
+		
+	});
 });
